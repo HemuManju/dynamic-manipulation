0b1b05d34399d8926560e82c163f75256c1bd4d3
Hemu <hemanthm2277@gmail.com>
2019-05-12 15:42:29 -0400

    Created repository

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Study of optimal control of hammering task using a robot and variable stiffness gripper',
+    author='Hemanth Manjunahthaa',
+    license='MIT',
+)
